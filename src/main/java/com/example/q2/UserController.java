7d8f9ca81317f1d82fa68e2fbf78a3f1cf22a385
krishanjit19 <krishanjit20_ug@ei.nits.ac.in>
2024-01-17 01:37:53 +0530

    Initial commit

---
+++ src/main/java/com/example/q2/UserController.java
@@ -0,0 +1,24 @@
+package com.example.q2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/info")
+    public String getUserInfo() {
+        userService.getInfo();
+        return "User information retrieved successfully!";
+    }
+}
---
+++ src/main/java/com/example/q2/UserService.java
@@ -0,0 +1,11 @@
+package com.example.q2;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public void getInfo() {
+        System.out.println("User information: [some value]");
+    }
+}
+
